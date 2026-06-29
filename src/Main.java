b599fc4dc75cb879c373c6db8bcc2952afe8ea29
Avina Paul <paul2aj@mail.uc.edu>
2026-06-29 13:52:04 -0400

    task 1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        // Task 1
+        for (int i = 0; i <= 30; i++) {
+            System.out.println(i);
+        }
+    }
+}
